3d0311c48d5817ac19844c2e005efbbb7cfd239a
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-06 21:54:36 -0400

    Confirming data binding works

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var app = anguarl.module('mist', []);
+var app = angular.module('mist', []);
 
 app.controller('mainController', function() {
 
   var self = this;
   self.message = "Hi!";
 
-}]);
+});
